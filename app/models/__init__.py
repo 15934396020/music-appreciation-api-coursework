622f72c6479be16c5063e8efef69a05975b5f503
ZeyuShen <136323744+15934396020@users.noreply.github.com>
2026-04-15 08:44:16 +0000

    Initialize models module with entities import

---
+++ app/models/__init__.py
@@ -0,0 +1,10 @@
+from app.models.entities import Collection, CollectionItem, Genre, Review, Track, UserTag
+
+__all__ = [
+    "Genre",
+    "Track",
+    "Review",
+    "UserTag",
+    "Collection",
+    "CollectionItem",
+]
